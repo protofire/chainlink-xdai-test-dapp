646a8db78a7cf249e663bd94a4e3641632f2d930
Felipe Buiras <f.morisan@gmail.com>
2020-12-09 20:11:51 -0300

    deploy fix

--- src/App.js
+++ src/App.js
@@ -9,16 +9,10 @@ import { Container } from 'react-bootstrap'
 
 function App() {
   const context = useWeb3Context()
-  const [blockNumber, setBlockNumber] = React.useState(null)
 
   React.useEffect(() => {
     if (!context.active) {
       context.setFirstValidConnector(['MetaMask'])
-    }
-
-    if (context.active) {
-      const { library } = context
-      library.getBlockNumber().then(setBlockNumber)
     }
   }, [context])
 
@@ -28,7 +22,6 @@ function App() {
   } else if (context.error) {
   content = <div>'There was an error: {context.error.toString()}'</div>
   } else {
-    //content = <div>{blockNumber ? `Block number: ${blockNumber.toString()}` : 'Fetching block number'}</div>
     content = <AggregatorState />
   }
 
